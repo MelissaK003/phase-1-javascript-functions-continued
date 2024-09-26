6f13ecef3401748f29eb7769c836918e26351bd0
Melissa <melissakiptoo@gmail.com>
2024-09-26 20:50:08 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+
+function saturdayFun(activity = "roller-skate") 
+    {return `This Saturday, I want to ${activity}!`;}
+
+console.log(saturdayFun('dance')) 
+
+function mondayWork(activity = "go to the office.") 
+    {return `This Monday, I will ${activity}`; }
+
+console.log(mondayWork("work from home."))
+
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`; };}
+
